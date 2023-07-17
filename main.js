f08677f921a61057ed78c14ee33635781f37d726
williamthecon <84906216+williamthecon@users.noreply.github.com>
2023-07-17 19:45:33 +0200

    Break save for books

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-function getElementsWidth(element) {
-    const style = window.getComputedStyle(element);
-    const width = element.clientWidth + parseFloat(style.marginLeft) + parseFloat(style.marginRight) + parseFloat(style.paddingLeft) + parseFloat(style.paddingRight) + parseFloat(style.borderLeftWidth) + parseFloat(style.borderRightWidth);
-    return width;
-}
+// function getElementsWidth(element) {
+//     const style = window.getComputedStyle(element);
+//     const width = element.clientWidth + parseFloat(style.marginLeft) + parseFloat(style.marginRight) + parseFloat(style.paddingLeft) + parseFloat(style.paddingRight) + parseFloat(style.borderLeftWidth) + parseFloat(style.borderRightWidth);
+//     return width;
+// }
 
-document.addEventListener("DOMContentLoaded", (e) => {
-    const topics = document.querySelectorAll(".topic");
-    let maxTopicMinWidth = parseInt(document.body.style.minWidth);
-    console.log("maxTopicMinWidth: " + maxTopicMinWidth);
-    topics.forEach(topic => {
-        const headerWidth = getElementsWidth(topic.querySelector(".topic-header"));
-        const buttonLinkWidth = getElementsWidth(topic.querySelector(".button-link"));
-        const topicWidth = getElementsWidth(topic);
-        const topicMinWidth = topicWidth - topic.clientWidth + headerWidth + buttonLinkWidth;
-        console.log("topicMinWidth: " + topicMinWidth);
-        if (topicMinWidth > maxTopicMinWidth) {
-            maxTopicMinWidth = topicMinWidth;
-        }
-    });
-    console.log("maxTopicMinWidth: " + maxTopicMinWidth);
-    document.body.style.minWidth = maxTopicMinWidth + "px";
-});
+// document.addEventListener("DOMContentLoaded", (e) => {
+//     const topics = document.querySelectorAll(".topic");
+//     let maxTopicMinWidth = parseInt(document.body.style.minWidth);
+//     console.log("maxTopicMinWidth: " + maxTopicMinWidth);
+//     topics.forEach(topic => {
+//         const headerWidth = getElementsWidth(topic.querySelector(".topic-header"));
+//         const buttonLinkWidth = getElementsWidth(topic.querySelector(".button-link"));
+//         const topicWidth = getElementsWidth(topic);
+//         const topicMinWidth = topicWidth - topic.clientWidth + headerWidth + buttonLinkWidth;
+//         console.log("topicMinWidth: " + topicMinWidth);
+//         if (topicMinWidth > maxTopicMinWidth) {
+//             maxTopicMinWidth = topicMinWidth;
+//         }
+//     });
+//     console.log("maxTopicMinWidth: " + maxTopicMinWidth);
+//     document.body.style.minWidth = maxTopicMinWidth + "px";
+// });
 
 // window.onresize = () => {
 //     const topics = document.querySelectorAll(".topic");

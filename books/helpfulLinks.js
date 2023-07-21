f19106dc67105b2b67b621f68e1b39ac261f419a
williamthecon <84906216+williamthecon@users.noreply.github.com>
2023-07-21 11:46:08 +0200

    Small Fixes - Added Database

---
+++ books/helpfulLinks.js
@@ -0,0 +1,27 @@
+function toggleFold() {
+    var div = document.getElementById("helpful-links");
+    var chevron_down = document.getElementById("chevron-down")
+    var chevron_up = document.getElementById("chevron-up")
+    if (div.style.display === "none") {
+        div.style.display = "block";
+        chevron_down.style.display = "none";
+        chevron_up.style.display = "inline-block";
+        localStorage.setItem('helpfulLinksFolded', "false");
+    } else {
+        div.style.display = "none";
+        chevron_down.style.display = "inline-block";
+        chevron_up.style.display = "none";
+        localStorage.setItem('helpfulLinksFolded', "true");
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const helpfulLinksFolded = localStorage.getItem('helpfulLinksFolded');
+    if (helpfulLinksFolded !== null) {
+        if (helpfulLinksFolded === "true") {
+            toggleFold();
+        }
+    } else {
+        localStorage.setItem('helpfulLinksFolded', "false");
+    }
+});
--- books/main.js
+++ books/main.js
@@ -23,30 +23,6 @@ function deleteLocalStorage(key) {
     localStorage.removeItem(key);
 }
 
-function toggleFold() {
-    var div = document.getElementById("helpful-links");
-    var chevron_down = document.getElementById("chevron-down")
-    var chevron_up = document.getElementById("chevron-up")
-    if (div.style.display === "none") {
-        div.style.display = "block";
-        chevron_down.style.display = "none";
-        chevron_up.style.display = "inline-block";
-        localStorage.setItem('helpfulLinksFolded', "false");
-    } else {
-        div.style.display = "none";
-        chevron_down.style.display = "inline-block";
-        chevron_up.style.display = "none";
-        localStorage.setItem('helpfulLinksFolded', "true");
-    }
+function logIn(username, password) {
+    setLocalStorage('username', username);
 }
-
-document.addEventListener("DOMContentLoaded", function() {
-    const helpfulLinksFolded = localStorage.getItem('helpfulLinksFolded');
-    if (helpfulLinksFolded !== null) {
-        if (helpfulLinksFolded === "true") {
-            toggleFold();
-        }
-    } else {
-        localStorage.setItem('helpfulLinksFolded', "false");
-    }
-});

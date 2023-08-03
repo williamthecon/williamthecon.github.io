b1f90d4b59edeaef07e9dc1de7b7203dadb986a7
williamthecon <84906216+williamthecon@users.noreply.github.com>
2023-08-03 11:38:49 +0200

    fetch exp.

--- books/main.js
+++ books/main.js
@@ -114,7 +114,21 @@ async function loadData(type, define) {
     return fetch("https://my-book-api.wtc248.repl.co/load/" + type, {
         method: 'GET',
         headers: {
-            'Content-Type': 'application/json; charset=utf-8'
+            'Content-Type': 'application/json; charset=utf-8',
+            'Host': 'my-book-api.wtc248.repl.co',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+            'Accept-Language': 'en-GB,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Prefer': 'safe',
+            'DNT': '1',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+            'Sec-Fetch-Dest': 'document',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-Site': 'none',
+            'Sec-Fetch-User': '?1',
+            'TE': 'trailers'
         }
     }).then(response => define(response.json())).catch(error => console.log(error));
 }

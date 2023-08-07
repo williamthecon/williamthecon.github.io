1df9458905c33d35482d38af652ffe28bca846db
williamthecon <84906216+williamthecon@users.noreply.github.com>
2023-08-07 19:30:42 +0200

    helpfulLinks.js to foldableDivs.js

--- books/foldableDivs.js
+++ books/foldableDivs.js
@@ -1,17 +1,17 @@
-function toggleFold() {
-    var div = document.getElementById("helpful-links");
-    var chevron_down = document.getElementById("chevron-down")
-    var chevron_up = document.getElementById("chevron-up")
+function toggleFold(id) {
+    var div = document.getElementById(id);
+    var chevron_down = document.getElementById(id + "--chevron-down")
+    var chevron_up = document.getElementById(id + "--chevron-up")
     if (div.style.display === "none") {
         div.style.display = "block";
         chevron_down.style.display = "none";
         chevron_up.style.display = "inline-block";
-        localStorage.setItem('helpfulLinksFolded', "false");
+        localStorage.setItem(id + "--folded", "false");
     } else {
         div.style.display = "none";
         chevron_down.style.display = "inline-block";
         chevron_up.style.display = "none";
-        localStorage.setItem('helpfulLinksFolded', "true");
+        localStorage.setItem(id + "--folded", "true");
     }
 }
 
@@ -22,12 +22,12 @@ function logout() {
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    const helpfulLinksFolded = localStorage.getItem('helpfulLinksFolded');
+    const helpfulLinksFolded = localStorage.getItem('helpful-links--folded');
     if (helpfulLinksFolded !== null) {
         if (helpfulLinksFolded === "true") {
-            toggleFold();
+            toggleFold("helpful-links");
         }
     } else {
-        localStorage.setItem('helpfulLinksFolded', "false");
+        localStorage.setItem('helpful-links--folded', "false");
     }
 });

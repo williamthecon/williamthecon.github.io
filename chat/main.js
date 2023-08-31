51ae616d8ecb94c4a0dc72da4f27f6ca9e6aa9ae
williamthecon <84906216+williamthecon@users.noreply.github.com>
2023-08-31 16:20:10 +0200

    /chat/username impr.

--- chat/main.js
+++ chat/main.js
@@ -1,4 +1,15 @@
+function getCookie(name) {
+    const cookies = document.cookie.split('; ');
+    for (const cookie of cookies) {
+        const [key, value] = cookie.split('=');
+        if (key === name) {
+            return value;
+        }
+    }
+    return null;
+}
+
 // Check if user has a session
-if (getCookie("session")) {
-    // Load
+if (!getCookie("session")) {
+    window.location.href = "/chat/username";
 }

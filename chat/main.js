8734659033842e3cf4fd0f3c2a4ad5f915eb3b56
williamthecon <84906216+williamthecon@users.noreply.github.com>
2023-09-02 18:11:56 +0200

    /chat/main.js temp. logg.

--- chat/main.js
+++ chat/main.js
@@ -9,6 +9,8 @@ function getCookie(name) {
     return null;
 }
 
+console.log(document.cookie);
+console.log(getCookie("sessionId"));
 // Check if user has a session
 // if (!getCookie("sessionId")) {
 //     window.location.href = "/chat/username";
